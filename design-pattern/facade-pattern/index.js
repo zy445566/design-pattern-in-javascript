3d0c2d6be1cf7c789b8932dc698f053c7027a6c4
zy445566 <zyct@vip.qq.com>
2019-11-19 11:31:17 +0800

    外观模式代码提交

---
+++ design-pattern/facade-pattern/index.js
@@ -0,0 +1,41 @@
+class Rectangle {
+    draw() {
+       console.log("Rectangle::draw()");
+    }
+}
+
+class Square {
+    draw() {
+       console.log("Square::draw()");
+    }
+}
+
+class Circle {
+    draw() {
+       console.log("Circle::draw()");
+    }
+}
+
+class ShapeMaker {
+    constructor() {
+        this.circle = new Circle();
+        this.rectangle = new Rectangle();
+        this.square = new Square();
+    }
+
+    drawCircle(){
+        this.circle.draw();
+    }
+    drawRectangle(){
+        this.rectangle.draw();
+    }
+    drawSquare(){
+        this.square.draw();
+    }
+}
+
+const shapeMaker = new ShapeMaker();
+
+shapeMaker.drawCircle();
+shapeMaker.drawRectangle();
+shapeMaker.drawSquare();
