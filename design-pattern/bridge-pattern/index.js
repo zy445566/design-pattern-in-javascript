040adae3ab44d6888a8f5f96ad3ebefbcd1dd4a2
zy445566 <zyct@vip.qq.com>
2019-11-19 10:09:24 +0800

    桥接模式代码添加

---
+++ design-pattern/bridge-pattern/index.js
@@ -0,0 +1,39 @@
+class RedCircle {
+    drawCircle(radius, x, y) {
+       console.log("Drawing Circle[ color: red, radius: "
+          + radius +", x: " +x+", "+ y +"]");
+    }
+}
+class GreenCircle {
+    drawCircle(radius, x, y) {
+       console.log("Drawing Circle[ color: green, radius: "
+          + radius +", x: " +x+", "+ y +"]");
+    }
+}
+
+class Shape {
+    constructor(drawAPI) {
+        if(new.target == Shape) {
+            throw new Error('this class must be extends.')
+        }
+        this.drawAPI = drawAPI;
+    }
+}
+
+class Circle extends Shape {
+    constructor(x, y, radius, drawAPI) {
+       super(drawAPI);
+       this.x = x;  
+       this.y = y;  
+       this.radius = radius;
+    }
+    draw() {
+        this.drawAPI.drawCircle(this.radius, this.x, this.y);
+    }
+}
+
+const redCircle = new Circle(100,100, 10, new RedCircle());
+const greenCircle = new Circle(100,100, 10, new GreenCircle());
+
+redCircle.draw();
+greenCircle.draw();
