4c8d29ea6e9d55886f4b10dfb7647bf8704f49c9
zy445566 <zyct@vip.qq.com>
2019-11-19 10:36:14 +0800

    添加过滤器模式代码

---
+++ design-pattern/filter-pattern/index.js
@@ -0,0 +1,116 @@
+// 定义对象
+class Person {
+    constructor(name, gender, maritalStatus){
+       this.name = name;
+       this.gender = gender;
+       this.maritalStatus = maritalStatus;    
+    }
+    getName() {
+       return this.name;
+    }
+    getGender() {
+       return this.gender;
+    }
+    getMaritalStatus() {
+       return this.maritalStatus;
+    }  
+}
+
+// 添加筛选条件
+class CriteriaMale {
+    meetCriteria(persons) {
+       const malePersons = [];
+       for (const person of persons) {
+          if(person.getGender().toUpperCase() == "MALE"){
+             malePersons.push(person);
+          }
+       }
+       return malePersons;
+    }
+}
+
+class CriteriaFemale {
+    meetCriteria(persons) {
+       const femalePersons = [];
+       for (const person of persons) {
+          if(person.getGender().toUpperCase() == "FEMALE"){
+             femalePersons.push(person);
+          }
+       }
+       return femalePersons;
+    }
+}
+
+class CriteriaSingle {
+    meetCriteria(persons) {
+       const singlePersons = [];
+       for (const person of persons) {
+          if(person.getMaritalStatus().toUpperCase() == "SINGLE"){
+             singlePersons.push(person);
+          }
+       }
+       return singlePersons;
+    }
+}
+
+// 添加筛选操作符
+class AndCriteria {
+    constructor(criteria, otherCriteria) {
+       this.criteria = criteria;
+       this.otherCriteria = otherCriteria;
+    }
+    meetCriteria(persons) {
+       const firstCriteriaPersons = this.criteria.meetCriteria(persons);
+       return this.otherCriteria.meetCriteria(firstCriteriaPersons);
+    }
+ }
+
+ class OrCriteria{
+    constructor(criteria, otherCriteria) {
+       this.criteria = criteria;
+       this.otherCriteria = otherCriteria;
+    }
+ 
+    meetCriteria(persons) {
+       const firstCriteriaItems = this.criteria.meetCriteria(persons);
+       const otherCriteriaItems = this.otherCriteria.meetCriteria(persons);
+       for (const person of otherCriteriaItems) {
+          if(firstCriteriaItems.indexOf(person)==-1){
+            firstCriteriaItems.push(person);
+          }
+       }
+       return firstCriteriaItems;
+    }
+ }
+
+function printPersons(persons){
+    for (const  person of persons) {
+       console.log(person);
+    }
+}
+
+const persons = [];
+persons.push(new Person("Robert","Male", "Single"));
+persons.push(new Person("John","Male", "Married"));
+persons.push(new Person("Laura","Female", "Married"));
+persons.push(new Person("Diana","Female", "Single"));
+persons.push(new Person("Mike","Male", "Single"));
+persons.push(new Person("Bobby","Male", "Single"));
+
+const male = new CriteriaMale();
+const female = new CriteriaFemale();
+const single = new CriteriaSingle();
+const singleMale = new AndCriteria(single, male);
+const singleOrFemale = new OrCriteria(single, female);
+
+console.log("Males: ");
+printPersons(male.meetCriteria(persons));
+
+console.log("\nFemales: ");
+printPersons(female.meetCriteria(persons));
+
+console.log("\nSingle Males: ");
+printPersons(singleMale.meetCriteria(persons));
+
+console.log("\nSingle Or Females: ");
+printPersons(singleOrFemale.meetCriteria(persons));
