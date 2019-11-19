60d548c5746e1fc8255d8326e3cc68afbbbcd013
zy445566 <zyct@vip.qq.com>
2019-11-19 14:55:00 +0800

    完成代理模式代码

---
+++ design-pattern/proxy-pattern/index.js
@@ -0,0 +1,62 @@
+class RealImage {
+    constructor(fileName){
+        this.fileName = fileName;
+        this.loadFromDisk(fileName);
+    }
+    display() {
+        console.log("Displaying " + this.fileName);
+    }
+    loadFromDisk(fileName){
+        console.log("Loading " + fileName);
+    }
+}
+
+class ProxyImage {
+    constructor(fileName){
+       this.fileName = fileName;
+    }
+    display() {
+       if(this.realImage == null){
+          this.realImage = new RealImage(this.fileName);
+       }
+       this.realImage.display();
+    }
+}
+
+const image = new ProxyImage("test_10mb.jpg");
+
+console.log('not use es6 Proxy:')
+// 图像将从磁盘加载
+image.display();
+console.log('')
+// 图像不需要从磁盘加载
+image.display();
+
+// 其实上面的代码可以直接用es6代替
+const ProxyImageEs6 = new Proxy(RealImage,{
+    construct: function(target, args) {
+        let imageObj = {};
+        imageObj.__proto__ = target.prototype;
+        imageObj.fileName = args[0];
+        return new Proxy(imageObj,{
+            get: function (target, key, receiver) {
+                if(key=='display') {
+                    return ()=>{
+                        if(this.realImage == null){
+                            this.realImage = new RealImage(target.fileName);
+                        }
+                        this.realImage.display();
+                    };
+                }
+                return Reflect.get(target, key, receiver);
+            },
+        });
+    }
+})
+console.log('\n\nuse es6 Proxy:')
+const imageEs6 = new ProxyImageEs6("test_10mb.jpg");
+// 图像将从磁盘加载
+imageEs6.display();
+console.log('')
+// 图像不需要从磁盘加载
+imageEs6.display();
