07ca18734ca93fc6dfc312c50aa366a42b35a354
zy445566 <zyct@vip.qq.com>
2019-11-19 15:24:42 +0800

    命令模式代码

---
+++ design-pattern/command-pattern/index.js
@@ -0,0 +1,62 @@
+class Stock {
+    constructor() {
+        this.name = "ABC";
+        this.quantity = 10;
+    }
+    buy(){
+       console.log("Stock [ Name: " + this.name + 
+       ",Quantity: " + this.quantity +" ] bought");
+    }
+    sell(){
+        console.log("Stock [ Name: " + this.name + 
+       ",Quantity: " + this.quantity +" ] sold");
+    }
+ }
+
+ class BuyStock {
+    constructor(abcStock){
+       this.abcStock = abcStock;
+    }
+    execute() {
+        this.abcStock.buy();
+    }
+}
+class SellStock{
+    constructor(abcStock){
+        this.abcStock = abcStock;
+    }
+    execute() {
+        this.abcStock.sell();
+    }
+}
+
+class Broker {
+    constructor() {
+        this.orderList = [];
+    }
+ 
+    takeOrder(order){
+       this.orderList.push(order);      
+    }
+ 
+    placeOrders(){
+       for (const order of this.orderList) {
+          order.execute();
+       }
+       this.orderList = [];
+    }
+}
+
+const abcStock = new Stock();
+
+const buyStockOrder = new BuyStock(abcStock);
+const sellStockOrder = new SellStock(abcStock);
+
+const broker = new Broker();
+broker.takeOrder(buyStockOrder);
+broker.takeOrder(buyStockOrder);
+broker.takeOrder(buyStockOrder);
+broker.takeOrder(sellStockOrder);
+broker.takeOrder(sellStockOrder);
+
+broker.placeOrders();
