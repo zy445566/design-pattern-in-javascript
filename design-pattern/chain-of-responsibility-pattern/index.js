72a8c900d7c08d2d7daf51682ff8bd6ef07189b5
zy445566 <zyct@vip.qq.com>
2019-11-19 15:15:56 +0800

    完成责任链代码

---
+++ design-pattern/chain-of-responsibility-pattern/index.js
@@ -0,0 +1,73 @@
+class AbstractLogger {
+    constructor() {
+        if(new.target == AbstractLogger) {
+            throw new Error('this class must be extends.')
+        }
+    }
+    setNextLogger(nextLogger){
+       this.nextLogger = nextLogger;
+    }
+    write(message) {}
+    // 对于形成链的数据，依次执行
+    logMessage(level, message){
+       if(this.level <= level){
+          this.write(message);
+       }
+       if(this.nextLogger !=null){
+          this.nextLogger.logMessage(level, message);
+       }
+    }
+}
+AbstractLogger.INFO = 1;
+AbstractLogger.DEBUG = 2;
+AbstractLogger.ERROR = 3;
+//定义错误类型
+class StandardLogger extends AbstractLogger {
+    constructor(level){
+        super()
+        this.level = level;
+    }
+    write(message) {    
+       console.log("Standard Console::Logger: " + message);
+    }
+}
+class ErrorLogger extends AbstractLogger {
+    constructor(level){
+        super()
+        this.level = level;
+    }
+    write(message) {    
+       console.log("Error Console::Logger: " + message);
+    }
+}
+class FileLogger extends AbstractLogger {
+    constructor(level){
+        super()
+        this.level = level;
+    }
+    write(message) {    
+       console.log("File Console::Logger: " + message);
+    }
+}
+
+function getChainOfLoggers(){
+    const errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+    const fileLogger = new FileLogger(AbstractLogger.DEBUG);
+    const standardLogger = new StandardLogger(AbstractLogger.INFO);
+
+    errorLogger.setNextLogger(fileLogger);
+    fileLogger.setNextLogger(standardLogger);
+
+    return errorLogger;  
+}
+
+const loggerChain = getChainOfLoggers();
+
+// 实现级别高的报错依次从当前级别输出到低级别
+loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+console.log('')
+loggerChain.logMessage(AbstractLogger.DEBUG,
+    "This is a debug level information.");
+console.log('')
+loggerChain.logMessage(AbstractLogger.ERROR,
+    "This is an error information.");
