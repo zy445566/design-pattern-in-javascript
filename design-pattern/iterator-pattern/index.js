4936af0b29371e41c25307fb212e6a26cb54aff7
zy445566 <zyct@vip.qq.com>
2019-11-20 11:11:17 +0800

    迭代器模式

---
+++ design-pattern/iterator-pattern/index.js
@@ -0,0 +1,60 @@
+
+
+class NameRepository {
+    constructor() {
+        this.names = ["Robert" , "John" ,"Julie" , "Lora"];
+    }
+    getIterator() {
+        const names = this.names;
+        class NameIterator{
+            constructor() {
+                this.index = 0;
+            }
+            hasNext() {
+                if(this.index < names.length){
+                    return true;
+                }
+                return false;
+            }
+        
+            next() {
+                if(this.hasNext()){
+                    return names[this.index++];
+                }
+                return null;
+            }     
+        }
+       return new NameIterator();
+    }
+}
+
+console.log("ES5 Iterator:");
+const namesRepository = new NameRepository();
+for(const iter = namesRepository.getIterator(); iter.hasNext();){
+    const name = iter.next();
+    console.log("Name : " + name);
+}
+
+// 而在es6中可以这样使用
+class NameRepositoryEs6 {
+    constructor() {
+        this.names = ["Robert" , "John" ,"Julie" , "Lora"];
+        this.index = 0;
+    }
+    [Symbol.iterator]() {
+        return {
+            next:  () => {
+                let done = true;
+                if(this.index < this.names.length){
+                    done = false;
+                }
+                return {value: this.names[this.index++],done};
+            }
+          };
+    }
+}
+console.log("\nES6 Iterator:");
+const namesRepositoryEs6 = new NameRepositoryEs6()
+for(const name of namesRepositoryEs6) {
+    console.log("Name : " + name);
+}
