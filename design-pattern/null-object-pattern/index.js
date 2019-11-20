678556e9c7a1b05f3dff6115b2bac77f4b267575
zy445566 <zyct@vip.qq.com>
2019-11-20 14:39:34 +0800

    空对象模式

---
+++ design-pattern/null-object-pattern/index.js
@@ -0,0 +1,42 @@
+class RealCustomer {
+    constructor(name) {
+       this.name = name;    
+    }
+    getName() {
+       return this.name;
+    }
+    isNil() {
+       return false;
+    }
+}
+class NullCustomer {
+    getName() {
+       return "Not Available in Customer Database";
+    }
+    isNil() {
+       return true;
+    }
+}
+
+class CustomerFactory {
+    static getCustomer(name){
+       for (let i = 0; i < CustomerFactory.names.length; i++) {
+          if (CustomerFactory.names[i].toUpperCase()==name.toUpperCase()){
+             return new RealCustomer(name);
+          }
+       }
+       return new NullCustomer();
+    }
+}
+CustomerFactory.names = ["Rob", "Joe", "Julie"];
+
+const customer1 = CustomerFactory.getCustomer("Rob");
+const customer2 = CustomerFactory.getCustomer("Bob");
+const customer3 = CustomerFactory.getCustomer("Julie");
+const customer4 = CustomerFactory.getCustomer("Laura");
+
+console.log("Customers");
+console.log(customer1.getName());
+console.log(customer2.getName());
+console.log(customer3.getName());
+console.log(customer4.getName());
