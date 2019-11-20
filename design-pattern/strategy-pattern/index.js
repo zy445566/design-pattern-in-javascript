3aed9e01892431cbe78913ca4e29103b170878ab
zy445566 <zyct@vip.qq.com>
2019-11-20 14:44:55 +0800

    策略模式

---
+++ design-pattern/strategy-pattern/index.js
@@ -0,0 +1,33 @@
+class OperationAdd {
+    doOperation(num1, num2) {
+       return num1 + num2;
+    }
+}
+class OperationSubstract {
+    doOperation(num1, num2) {
+       return num1 - num2;
+    }
+}
+class OperationMultiply {
+    doOperation(num1, num2) {
+       return num1 * num2;
+    }
+}
+
+class Context {
+    constructor(strategy){
+       this.strategy = strategy;
+    }
+    executeStrategy(num1, num2){
+       return this.strategy.doOperation(num1, num2);
+    }
+}
+
+let context = new Context(new OperationAdd());    
+console.log("10 + 5 = " + context.executeStrategy(10, 5));
+
+context = new Context(new OperationSubstract());      
+console.log("10 - 5 = " + context.executeStrategy(10, 5));
+
+context = new Context(new OperationMultiply());    
+console.log("10 * 5 = " + context.executeStrategy(10, 5));
