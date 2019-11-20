927f019f311db9d2584f88b05b9fc4a06b5ecaa0
zy445566 <zyct@vip.qq.com>
2019-11-20 15:07:31 +0800

    访问者模式

---
+++ design-pattern/visitor-pattern/index.js
@@ -0,0 +1,36 @@
+class Keyboard {
+    accept(computerPartVisitor) {
+       computerPartVisitor.visit(this);
+    }
+}
+class Monitor {
+    accept(computerPartVisitor) {
+       computerPartVisitor.visit(this);
+    }
+}
+class Mouse {
+    accept(computerPartVisitor) {
+       computerPartVisitor.visit(this);
+    }
+}
+
+class Computer {
+    constructor(){
+       this.parts = [new Mouse(), new Keyboard(), new Monitor()];      
+    } 
+    accept(computerPartVisitor) {
+       for (let i = 0; i < this.parts.length; i++) {
+        this.parts[i].accept(computerPartVisitor);
+       }
+       computerPartVisitor.visit(this);
+    }
+}
+
+class ComputerPartDisplayVisitor{
+    visit(device) {
+        console.log(`Displaying ${device.constructor.name}.`);
+    }
+}
+
+const computer = new Computer();
+computer.accept(new ComputerPartDisplayVisitor());
