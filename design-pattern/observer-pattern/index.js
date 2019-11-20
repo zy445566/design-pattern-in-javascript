17578a4efe52c3cda33dd8eea9e704fcdf06c140
zy445566 <zyct@vip.qq.com>
2019-11-20 14:19:28 +0800

    观察者模式

---
+++ design-pattern/observer-pattern/index.js
@@ -0,0 +1,63 @@
+class Subject {
+    constructor() {
+        this.observers = [];
+    }
+    getState() {
+       return this.state;
+    }
+    setState(state) {
+       this.state = state;
+       this.notifyAllObservers();
+    }
+    attach(observer){
+       this.observers.push(observer);      
+    }
+    notifyAllObservers(){
+       for (const observer of this.observers) {
+          observer.update();
+       }
+    }  
+ }
+
+class BinaryObserver {
+    constructor(subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+    update() {
+        console.log( "Binary String: " 
+        + this.subject.getState().toString(2) ); 
+    }
+}
+class OctalObserver {
+    constructor(subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+    update() {
+        console.log( "Octal String: " 
+        + this.subject.getState().toString(8) ); 
+    }
+}
+
+class HexObserver {
+    constructor(subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+    update() {
+        console.log( "Hex String: " 
+        + this.subject.getState().toString(16) ); 
+    }
+}
+
+const subject = new Subject();
+ 
+new HexObserver(subject);
+new OctalObserver(subject);
+new BinaryObserver(subject);
+
+console.log("First state change: 15");   
+subject.setState(15);
+console.log("Second state change: 10");  
+subject.setState(10);
