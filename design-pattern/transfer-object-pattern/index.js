aa0aaa9453a33fbe850fa25b39e4f48e0ee61707
zy445566 <zyct@vip.qq.com>
2019-11-21 14:28:30 +0800

    传输对象模式代码

---
+++ design-pattern/transfer-object-pattern/index.js
@@ -0,0 +1,77 @@
+class StudentVO {
+    constructor(name, rollNo){
+       this.name = name;
+       this.rollNo = rollNo;
+    }
+    getName() {
+       return this.name;
+    }
+    setName(name) {
+       this.name = name;
+    }
+    getRollNo() {
+       return this.rollNo;
+    }
+    setRollNo(rollNo) {
+       this.rollNo = rollNo;
+    }
+}
+
+class StudentBO{
+    constructor(){
+        //列表是当作一个数据库
+        this.students = [];
+        this.students.getIndexByRollNo = (rollNo)=>{
+            return this.students.findIndex(
+                (val)=>val.getRollNo() == rollNo
+            );
+        }
+        const student1 = new StudentVO("Robert",0);
+        const student2 = new StudentVO("John",1);
+        this.students.push(student1);
+        this.students.push(student2);    
+    }
+    deleteStudent(student) {
+        this.students.splice(student.getIndexByRollNo(student.getRollNo() ),1);
+       console.log("Student: Roll No " + student.getRollNo() 
+          +", deleted from database");
+    }
+    //从数据库中检索学生名单
+    getAllStudents() {
+       return this.students;
+    }
+    getStudent(rollNo) {
+       return this.students[this.students.getIndexByRollNo(rollNo)];
+    }
+  
+    updateStudent(student) {
+        this.students[this.students.getIndexByRollNo(student.getRollNo())].setName(student.getName());
+       console.log("Student: Roll No " + student.getRollNo() 
+          +", updated in the database");
+    }
+}
+
+const studentBusinessObject = new StudentBO();
+ 
+//输出所有的学生
+for (const student of studentBusinessObject.getAllStudents()) {
+    console.log("Student: [RollNo : "
+    +student.getRollNo()+", Name : "+student.getName()+" ]");
+}
+
+//更新学生
+const student =studentBusinessObject.getAllStudents()[
+    studentBusinessObject.getAllStudents().getIndexByRollNo(0)
+];
+student.setName("Michael");
+studentBusinessObject.updateStudent(student);
+
+//获取学生
+studentBusinessObject.getStudent(0);
+console.log("Student: [RollNo : "
++student.getRollNo()+", Name : "+student.getName()+" ]");
+
+/**
+ * 这个传输对象模式和数据访问对象模式很相似
+ * 区别是数据访问对象模式通过定义接口获取，而传输对象模式通过打包业务对象服务获取
+ */
