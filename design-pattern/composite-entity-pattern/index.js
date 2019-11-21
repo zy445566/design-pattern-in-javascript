70dbe3a15d92dd2b6bc94443678d7ed57ff25bb3
zy445566 <zyct@vip.qq.com>
2019-11-21 10:56:36 +0800

    组合实体模式

---
+++ design-pattern/composite-entity-pattern/index.js
@@ -0,0 +1,64 @@
+class DependentObject1 {
+    setData(data){
+       this.data = data; 
+    } 
+    getData(){
+       return this.data;
+    }
+}
+
+class DependentObject2 {
+    setData(data) {
+       this.data = data; 
+    } 
+    getData() {
+       return this.data;
+    }
+}
+
+class CoarseGrainedObject {
+    constructor() {
+        this.do1 = new DependentObject1();
+        this.do2 = new DependentObject2();
+    }
+    
+    setData(data1, data2){
+        this.do1.setData(data1);
+        this.do2.setData(data2);
+    }
+    getData(){
+       return [this.do1.getData(),this.do2.getData()];
+    }
+}
+
+class CompositeEntity {
+    constructor() {
+        this.cgo = new CoarseGrainedObject();
+    }
+    setData(data1, data2){
+        this.cgo.setData(data1, data2);
+    }
+    getData(){
+       return this.cgo.getData();
+    }
+}
+
+class Client {
+    constructor() {
+        this.compositeEntity = new CompositeEntity();
+    }
+    printData(){
+       for (let i = 0; i < this.compositeEntity.getData().length; i++) {
+          console.log("Data: " + this.compositeEntity.getData()[i]);
+       }
+    }
+    setData(data1, data2){
+       this.compositeEntity.setData(data1, data2);
+    }
+}
+
+const client = new Client();
+client.setData("Test", "Data");
+client.printData();
+client.setData("Second Test", "Data1");
+client.printData();
